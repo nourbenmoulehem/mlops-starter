b5c45b5a34a08f646d606ecd0a2b7ee0364fc0fe
nourbenmoulehem <nbenmoulehom@gmail.com>
2026-05-26 12:57:13 +0100

    add metrics monitoring for data drift

--- serve.py
+++ serve.py
@@ -3,11 +3,35 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 
+from prometheus_client import Histogram, Counter, make_asgi_app
+
+
 app = FastAPI()
 
 # load the latest saved model from mlflow, currently in Production (champion alias)
 model = mlflow.sklearn.load_model("models:/iris-classifier@champion")
 
+
+metrics_app = make_asgi_app() # mini app that declares the metrics and format them for prometheus
+app.mount("/metrics", metrics_app)
+
+
+# now we're declaring our metrics:
+sepal_length_hist = Histogram(
+    "iris_sepal_length_cm",
+    "Sepal length distribution",
+    buckets=[4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0]
+)
+petal_length_hist = Histogram(
+    "iris_petal_length_cm",
+    "Petal length distribution",
+    buckets=[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+)
+prediction_counter = Counter(
+    "iris_predictions_total",
+    "Predictions by class",
+    ["class_name"]
+)
 
 
 class IrisInput(BaseModel):
@@ -28,7 +52,15 @@ def predict(data: IrisInput):
         data.petal_length,
         data.petal_width
     ]])
+
+    sepal_length_hist.observe(data.sepal_length)
+    petal_length_hist.observe(data.petal_length)
+
+
     prediction = model.predict(features)[0]
+
+    prediction_counter.labels(class_name=CLASSES[prediction]).inc()
+
     return {
         "prediction": int(prediction),
         "class_name": CLASSES[prediction]
